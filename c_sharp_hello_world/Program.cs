4f522ebf440c9ce5d111f290681e5f766956c521
Allan Castro <elcapi@Mac.home.lla.com>
2024-12-15 00:22:51 -0600

    Initial commit

---
+++ c_sharp_hello_world/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
